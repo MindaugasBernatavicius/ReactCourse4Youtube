6a222ef9bb7bbfebcc1de2fb6011a3a30652a831
MindaugasBernatavicius <darbas.mindaugas@gmail.com>
2020-03-24 11:50:21 +0200

    Trying github pages with react

---
+++ build/precache-manifest.e83124bfbec76ad63977a14ff4114ab9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e36977a740d8e73daa9e0a816a7f9bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "910fc420526b6b2a8d54",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "164d9e9b519b9213b750",
+    "url": "/static/js/2.9a33b7d4.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.9a33b7d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "910fc420526b6b2a8d54",
+    "url": "/static/js/main.1cfe8a08.chunk.js"
+  },
+  {
+    "revision": "7a2f3dd4f9938f3a2fa8",
+    "url": "/static/js/runtime-main.61deab09.js"
+  }
+]);
